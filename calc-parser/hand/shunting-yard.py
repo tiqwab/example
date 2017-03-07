b80c3623c30575e1752aac6460f26f33520cfc6f
tiqwab <tiqwab.ch90@gmail.com>
2017-03-07 21:42:59 +0900

    Implement add, sub, mult, div of shunting-yard parser

--- calc-parser/hand/shunting-yard.py
+++ calc-parser/hand/shunting-yard.py
@@ -1,96 +1,90 @@
-from enum import Enum, unique
+from collections import namedtuple
+from enum import Enum
+import re
+
+class Operator(namedtuple('Operator', ['symbol', 'func', 'precedence', 'associativity'])):
+    __slots__ = ()
 
 class Assoc(Enum):
-    Left  = 1,
-    Right = 2
+    Left = 0
+    Right = 1
 
-@unique
-class CalcOp(Enum):
-    plus  = ('+', lambda x,y: x+y, 1, Assoc.Left)
-    minus = ('-', lambda x,y: x-y, 1, Assoc.Left)
-    mult  = ('*', lambda x,y: x*y, 2, Assoc.Left)
-    div   = ('/', lambda x,y: x/y, 2, Assoc.Left)
-    pw    = ('^', lambda x,y: pow(x,y), 3, Assoc.Right)
+operators = (
+        Operator('+', lambda x, y: x + y, 1, Assoc.Left),
+        Operator('-', lambda x, y: x - y, 1, Assoc.Left),
+        Operator('*', lambda x, y: x * y, 2, Assoc.Left),
+        Operator('/', lambda x, y: x / y, 2, Assoc.Left),
+        Operator('**', lambda x, y: x ** y, 3, Assoc.Right),
+        )
 
-    def __init__(self, symbol, func, precedence, assoc):
-        self.symbol = symbol
-        self.func = func
-        self.precedence = precedence
-        self.assoc = assoc
+def find_op(sym):
+    ops = list(filter(lambda op: op.symbol == sym, operators))
+    if len(ops) == 0:
+        raise RuntimeError('illegal operator: ' + str(sym))
+    return ops[0]
 
-    @classmethod
-    def token_to_op(self, token):
-        for op in CalcOp:
-            if op.symbol == token:
-                return op
-        return None
+def can_apply_prev_op(prev_op, current_op):
+    if current_op.associativity == Assoc.Left:
+        return prev_op.precedence >= current_op.precedence
+    else:
+        return prev_op.precedence > current_op.precedence
 
 class Tokenizer:
-    def __init__(self, src):
-        self.src = src
-        self.src_ind = 0
+    def __init__(self, input):
+        self.tokens = input.split()
+        self.index = 0
 
     def next_token(self):
-        if self.src_ind >= len(self.src):
+        if self.index >= len(self.tokens):
             return None
-        token = self.src[self.src_ind]
-        self.src_ind += 1
+        token = self.tokens[self.index]
+        if re.match(r'^\d+$', token):
+            token = int(token)
+        self.index += 1
         return token
 
-def parse_op(op_token, outputs):
-    op_info = CalcOp.token_to_op(op_token)
-    e2 = outputs.pop()
-    e1 = outputs.pop()
-    outputs.append(op_info.func(int(e1), int(e2)))
+class ShuntingYardParser:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
 
-def compare_prec(cur_op, prev_op):
-    cur_op_info = CalcOp.token_to_op(cur_op)
-    prev_op_info = CalcOp.token_to_op(prev_op)
-    if cur_op_info is None or prev_op_info is None:
-        return False
-    if prev_op_info.assoc == Assoc.Left:
-        return prev_op_info.precedence >= cur_op_info.precedence
-    else:
-        return prev_op_info.precedence > cur_op_info.precedence
-
-def parse_to_rpn(src):
-    tokenizer = Tokenizer(src)
-    outputs = []
-    ops = []
-
-    while True:
-        token = tokenizer.next_token()
-        # after reading all input
-        if token is None:
-            while len(ops) > 0:
-                parse_op(ops.pop(), outputs)
-            break
-        op_info = CalcOp.token_to_op(token)
-        # process lparen
-        if token == '(':
-            ops.append(token)
-        # process rparen
-        elif token == ')':
+    def parse(self):
+        output = []
+        opstack = []
+        while True:
+            token = self.tokenizer.next_token()
+            if token is None:
+                break
+            if isinstance(token, int):
+                output.append(token)
+                continue
+            current_op = find_op(token)
             while True:
-                op = ops.pop()
-                if op == '(':
+                if len(opstack) == 0:
                     break
-                else:
-                    parse_op(op, outputs)
-        # Process operator
-        elif op_info is not None:
-            while len(ops) > 0:
-                if compare_prec(token, ops[len(ops)-1]):
-                    parse_op(ops.pop(), outputs)
-                else:
+                prev_op = opstack.pop()
+                if not can_apply_prev_op(prev_op, current_op):
+                    opstack.append(prev_op)
                     break
-            ops.append(token)
-        # Process operand
-        else:
-            outputs.append(token)
-    return outputs
+                rhs = output.pop()
+                lhs = output.pop()
+                output.append(prev_op.func(lhs, rhs))
+            opstack.append(current_op)
+        while len(opstack) > 0:
+            prev_op = opstack.pop()
+            rhs = output.pop()
+            lhs = output.pop()
+            output.append(prev_op.func(lhs, rhs))
+        return output[0]
+
+
+def calculate(input):
+    tokenizer = Tokenizer(input)
+    parser = ShuntingYardParser(tokenizer)
+    return parser.parse()
 
 if __name__ == '__main__':
-    print(parse_to_rpn('5*(4+3*2)+1'))
-    print(parse_to_rpn('5*(4-3*2)+6/2'))
-    print(parse_to_rpn('1+5*4^3^2')) # 1310721
+    assert calculate('1 + 2 + 3') == 6
+    assert calculate('1 * 2 + 3') == 5
+    assert calculate('1 + 2 * 3') == 7
+    assert calculate('1 - 6 * 3 / 2 + 4') == -4
+    assert calculate('1 + 3 * 2 ** 3 ** 2 - 4') == 1 + 3 * 512 - 4

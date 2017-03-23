ba31f70687cb3a1fafe96ed3d237a99796f445f2
tiqwab <tiqwab.ch90@gmail.com>
2017-03-23 21:01:19 +0900

    Replace repository with PagingAndSortingRepository

--- boot-with-react/src/main/java/com/tiqwab/example/payroll/DatabaseLoader.java
+++ boot-with-react/src/main/java/com/tiqwab/example/payroll/DatabaseLoader.java
@@ -19,7 +19,12 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        this.repository.save(new Employee("Taro", "Yamada", "this is Yamada"));
+        this.repository.save(new Employee("Aiko", "Aida", "this is Aida"));
+        this.repository.save(new Employee("Biko", "Bida", "this is Bida"));
+        this.repository.save(new Employee("Ciko", "Cida", "this is Cida"));
+        this.repository.save(new Employee("Diko", "Dida", "this is Dida"));
+        this.repository.save(new Employee("Eiko", "Eida", "this is Eida"));
+        this.repository.save(new Employee("Fiko", "Fida", "this is Fida"));
     }
 
 }
--- boot-with-react/src/main/java/com/tiqwab/example/payroll/EmployeeRepository.java
+++ boot-with-react/src/main/java/com/tiqwab/example/payroll/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package com.tiqwab.example.payroll;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 }

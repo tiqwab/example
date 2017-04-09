f99b50362e06289f8ac0282220e0906dae579668
tiqwab <tiqwab.ch90@gmail.com>
2017-04-09 21:29:28 +0900

    Add a simple rest-controller

--- boot-with-security/src/main/java/com/tiqwab/example/DemoApplication.java
+++ boot-with-security/src/main/java/com/tiqwab/example/DemoApplication.java
@@ -2,9 +2,24 @@ package com.tiqwab.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+	    Map<String, Object> model = new HashMap<>();
+	    model.put("id", UUID.randomUUID().toString());
+	    model.put("content", "Hello World");
+	    return model;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- boot-with-security/src/main/js/app.js
+++ boot-with-security/src/main/js/app.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 console.log('hello');
+
+const client = axios.create({
+    baseURL: 'http://localhost:8080/',
+});
+
+client.get('/resource')
+.then(response => {
+    console.log(response);
+})
+.catch(response => {
+    console.error(response);
+});

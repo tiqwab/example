4c327be0e16d0f496efaa4e787d8231f072f5662
tiqwab <tiqwab.ch90@gmail.com>
2017-04-10 23:07:59 +0900

    Add in-memory pairs of user and password

--- boot-with-security/src/main/java/com/tiqwab/example/SecurityConfig.java
+++ boot-with-security/src/main/java/com/tiqwab/example/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.tiqwab.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,5 +31,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutUrl("/logout")
                     .logoutSuccessUrl("/");
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                    .withUser("user").password("user").roles("USER")
+                    .and()
+                    .withUser("admin").password("admin").roles("ADMIN")
+                ;
+    }
+
+    // This is the configuration for global AuthenticationManager (https://spring.io/guides/topicals/spring-security-architecture/)
+    // I am not sure which should be used...
+    /*
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN")
+        ;
+
+    }
+    */
 
 }

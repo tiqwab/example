bbdaf2d4bf32fa30a3cb3713c1c19d39800c2efa
tiqwab <tiqwab.ch90@gmail.com>
2017-04-11 22:01:17 +0900

    Remove unnecessary import

--- boot-with-security/src/main/java/com/tiqwab/example/DemoApplication.java
+++ boot-with-security/src/main/java/com/tiqwab/example/DemoApplication.java
@@ -2,20 +2,6 @@ package com.tiqwab.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 @SpringBootApplication
 public class DemoApplication {

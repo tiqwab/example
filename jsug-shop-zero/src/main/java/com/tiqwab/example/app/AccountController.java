9ac2d2411a7478fa4a4cd46947d497c0cf318e91
tiqwab <tiqwab.ch90@gmail.com>
2017-04-12 22:54:13 +0900

    Just add a very simple createForm

---
+++ jsug-shop-zero/src/main/java/com/tiqwab/example/app/AccountController.java
@@ -0,0 +1,60 @@
+package com.tiqwab.example.app;
+
+import com.tiqwab.example.domain.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("account")
+public class AccountController {
+
+    /*
+    @Autowired
+    AccountService accountService;
+    */
+
+    // TODO: Check @ModelAttribute
+    // When is a model created and deleted?
+    @ModelAttribute
+    AccountForm setupForm() {
+        return new AccountForm();
+    }
+
+    @RequestMapping(value = "create", params = "form", method = RequestMethod.GET)
+    String createForm() {
+        return "account/createForm";
+    }
+
+    /*
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    String create(@Validated AccountForm form, BindingResult bindingResult, RedirectAttributes attributes) {
+        if (bindingResult.hasErrors()) {
+            return "account/createForm";
+        }
+        Account account = Account.builder()
+                .name(form.getName())
+                .password(Account.PASSWORD_ENCODER.encode(form.getPassword()))
+                .email(form.getEmail())
+                .birthDay(form.getBirthDay())
+                .zip(form.getZip())
+                .address(form.getAddress())
+                .age(form.getAge())
+                .build();
+        accountService.register(account);
+        attributes.addFlashAttribute(account);
+        return "redirect:/account/create?finish"; // post-request-get (PRG) pattern
+    }
+
+    @RequestMapping(value = "create", params = "finish", method = RequestMethod.GET)
+    String createFinish() {
+        return "account/createFinish";
+    }
+    */
+
+}
---
+++ jsug-shop-zero/src/main/java/com/tiqwab/example/app/AccountForm.java
@@ -0,0 +1,23 @@
+package com.tiqwab.example.app;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountForm {
+
+    private String name;
+    private String password;
+    private String email;
+    private LocalDate birthDay;
+    private String zip;
+    private String address;
+    private Integer age;
+    private String[] roles;
+
+}

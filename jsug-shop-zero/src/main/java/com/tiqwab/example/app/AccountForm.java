aeb0b07935399fb85e17920ca1c88430a4ed2974
tiqwab <tiqwab.ch90@gmail.com>
2017-04-13 20:10:55 +0900

    Add DateTimeFormat annotation for AccountForm

--- jsug-shop-zero/src/main/java/com/tiqwab/example/app/AccountForm.java
+++ jsug-shop-zero/src/main/java/com/tiqwab/example/app/AccountForm.java
@@ -3,6 +3,7 @@ package com.tiqwab.example.app;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -15,6 +16,9 @@ public class AccountForm {
     private String password;
     private String confirmPassword;
     private String email;
+    // TODO: @DataTimeFormat specifies format of date
+    // Use 'pattern' attribute if the more flexible pattern is necessary.
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birthDay;
     private String zip;
     private String address;

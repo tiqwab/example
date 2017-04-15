a0c3b687f8bf1e8823b55d6b8324e177804657a0
tiqwab <tiqwab.ch90@gmail.com>
2017-04-15 22:18:32 +0900

    Add @EntityScan

--- jsug-shop-zero/src/main/java/com/tiqwab/example/DemoApplication.java
+++ jsug-shop-zero/src/main/java/com/tiqwab/example/DemoApplication.java
@@ -2,11 +2,15 @@ package com.tiqwab.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {DemoApplication.class, Jsr310JpaConverters.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }

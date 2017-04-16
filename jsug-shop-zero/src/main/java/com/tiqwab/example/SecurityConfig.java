66a9a3a84b4b75c5c9572efc5cef36f8804d67ac
tiqwab <tiqwab.ch90@gmail.com>
2017-04-16 22:07:36 +0900

    Enable form login

---
+++ jsug-shop-zero/src/main/java/com/tiqwab/example/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.tiqwab.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/js/**", "/css/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .defaultSuccessUrl("/", true)
+                    .permitAll()
+                    .and()
+                .httpBasic()
+                    .and()
+                .logout()
+        ;
+    }
+
+}

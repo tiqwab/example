667c3de6cb42a1bdedd66672e37ac645e566a4a1
tiqwab <tiqwab.ch90@gmail.com>
2017-07-01 12:54:35 +0900

    Cookbook for zabbix-agent

---
+++ sample-zabbix-opsworks/cookbooks/zabbix-agent/README.md
@@ -0,0 +1,4 @@
+# zabbix-agent
+
+TODO: Enter the cookbook description here.
+
---
+++ sample-zabbix-opsworks/cookbooks/zabbix-agent/attributes/agent.rb
@@ -0,0 +1,2 @@
+default['zabbix-agent']['server'] = '127.0.0.1'
+default['zabbix-agent']['server-active'] = '127.0.0.1'

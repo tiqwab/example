b9309584874e52105f8955367b8e570b2f5efd50
tiqwab <tiqwab.ch90@gmail.com>
2017-11-25 15:54:27 +0900

    Delete akka-http-and-stream-sample

--- akka-http-and-stream-sample/src/main/scala/com/tiqwab/example/akka/ListMessageResponse.scala
+++
@@ -1,9 +0,0 @@
-package com.tiqwab.example.akka
-
-import spray.json._
-
-case class ListMessageResponse(messages: Seq[Message])
-
-object ListMessageResponse extends DefaultJsonProtocol {
-  implicit val jsonFormat: RootJsonFormat[ListMessageResponse] = jsonFormat1(ListMessageResponse.apply)
-}

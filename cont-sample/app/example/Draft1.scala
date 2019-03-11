d77d07e3acba6b4814cd8c06c7d03a0671f058a3
tiqwab (Naohisa Murakami) <nao-murakami@i-mobile.co.jp>
2019-03-11 19:39:09 +0900

    Update cont-sample

--- cont-sample/app/example/Draft1.scala
+++ cont-sample/app/example/Draft1.scala
@@ -94,4 +94,68 @@ object Draft1 {
     loop(fileNames, List.empty[MyFile])
   }
 
+  lazy val sample2: Option[Int] =
+    Option(1).flatMap { x =>
+      Option(x + 1).flatMap { y =>
+        Option(y * 2)
+      }
+    }
+
+  lazy val sample3: Option[Int] =
+    for {
+      x <- Option(1)
+      y <- Option(x + 1)
+    } yield y * 2
+
+  object MonadExercise {
+    import scala.language.higherKinds
+
+    trait Monad[F[_]] {
+      def pure[A](a: A): F[A]
+      def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+    }
+
+    sealed trait MyOption[+A] {}
+    case class MySome[+A](value: A) extends MyOption[A]
+    case object MyNone extends MyOption[Nothing]
+
+    object MyOption {
+      def apply[A](a: A): MyOption[A] = MySome(a)
+    }
+
+    implicit def optionMonad: Monad[Option] = new Monad[Option] {
+      override def pure[A](a: A): Option[A] = Some(a)
+      override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+    }
+
+    implicit def myOptionMonad: Monad[MyOption] = new Monad[MyOption] {
+      override def pure[A](a: A): MyOption[A] = MySome(a)
+      override def flatMap[A, B](fa: MyOption[A])(f: A => MyOption[B]): MyOption[B] =
+        fa match {
+          case MySome(v) => f(v)
+          case MyNone    => MyNone
+        }
+    }
+
+    implicit final class MyOptionOps[+A](opt: MyOption[A]) {
+      def flatMap[B](f: A => MyOption[B])(implicit F: Monad[MyOption]): MyOption[B] =
+        F.flatMap(opt)(f)
+
+      def map[B](f: A => B)(implicit F: Monad[MyOption]): MyOption[B] =
+        F.flatMap(opt)(x => F.pure(f(x)))
+    }
+
+    // ref. https://qiita.com/pab_tech/items/fc3d160a96cecdead622
+    case class Cont[R, A](run: (A => R) => R) {
+      def flatMap[B](f: A => Cont[R, B]): Cont[R, B] =
+        Cont(g => run(a => f(a).run(g)))
+      def map[B](f: A => B): Cont[R, B] =
+        flatMap(x => Cont.pure(f(x)))
+    }
+
+    object Cont {
+      def pure[R, A](a: A): Cont[R, A] = Cont(f => f(a))
+    }
+  }
+
 }

8dfab6b2ea9875ac9d396c733ac0b2836edb0967
tiqwab <tiqwab.ch90@gmail.com>
2021-05-30 10:49:32 +0900

    protobuf-sample

---
+++ protobuf-sample/go.mod
@@ -0,0 +1,8 @@
+module example.com/protobuf-sample
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.0
+	google.golang.org/protobuf v1.26.0
+)
---
+++ protobuf-sample/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	pb "example.com/protobuf-sample/gen"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"io/ioutil"
+	"os"
+)
+
+func simple_write(fileName string) error {
+	p := pb.Person{
+		Id:    1234,
+		Name:  "John Doe",
+		Email: "jdoe@example.com",
+		Phones: []*pb.Person_PhoneNumber{
+			{Number: "554-4321", Type: pb.Person_HOME},
+		},
+	}
+
+	book := &pb.AddressBook{}
+	book.People = append(book.People, &p)
+
+	out, err := proto.Marshal(book)
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(fileName, out, 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func simple_read(fileName string) (*pb.AddressBook, error) {
+	buf, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	var book pb.AddressBook
+	if err := proto.Unmarshal(buf, &book); err != nil {
+		return nil, err
+	}
+
+	return &book, nil
+}
+
+func main() {
+	const fileName = "addressbook.data"
+
+	if err := simple_write(fileName); err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+	book, err := simple_read(fileName)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%v\n", book)
+}

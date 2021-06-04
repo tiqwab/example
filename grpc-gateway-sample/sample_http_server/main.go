f321502d4f1ae7e7c269fd939b0fb7b061b6f706
tiqwab <tiqwab.ch90@gmail.com>
2021-06-04 09:28:33 +0900

    Use mux from grpc-gateway directly

--- grpc-gateway-sample/sample_http_server/main.go
+++ grpc-gateway-sample/sample_http_server/main.go
@@ -1,22 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a server for Greeter service.
 package main
 
 import (
@@ -70,8 +51,9 @@ func main() {
 	pb.RegisterGreeterHandlerServer(ctx, gw_mux, &server{})
 	pb.RegisterEchoHandlerServer(ctx, gw_mux, &server{})
 
-	mux := http.NewServeMux()
-	mux.Handle("/v1/", gw_mux)
+	// mux := http.NewServeMux()
+	// mux.Handle("/v1/", gw_mux)
+	mux := gw_mux
 
 	server := http.Server{
 		Handler: mux,
